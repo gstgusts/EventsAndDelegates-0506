654d7eacc3a1ba7742f491ed32b3e2c93ab26405
Gusts Linkevics <gusts@gst.lv>
2023-07-17 18:49:30 +0300

    Add project files.

---
+++ EventsAndDelegates-0506/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates_0506
+{
+    public class Order
+    {
+        public void Create()
+        {
+            Console.WriteLine("Order created");
+            EmailService.Send();
+            SmsService.Send();
+        }
+    }
+}
---
+++ EventsAndDelegates-0506/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EventsAndDelegates_0506
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order();
+            order.Create();
+        }
+    }
+}
